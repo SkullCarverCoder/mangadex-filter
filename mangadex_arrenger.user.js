a3d538bb41f54a004f617086ec60f4ddbdd1178e
Juan Hernandez <juanlh182@gmail.com>
2019-06-15 11:47:03 -0400

    it should have been...

--- mangadex_arrenger.user.js
+++ mangadex_arrenger.user.js
@@ -81,27 +81,27 @@
         }
       });
 
-      // inject Scanlation dropdown
-      $('div.col.order-lg-5 span.fas.fa-users').before('<div class="btn-group"><button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button><div>');
-      $('div.col.order-lg-5 button.btn.dropdown-toggle').append($('div.col.order-lg-5 span.fas.fa-users'));
-      $('div.col.order-lg-5 button.btn.dropdown-toggle span.fas.fa-users').after('<div  id="sortbyscan" class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position:absolute;will-change: transform; top:0px;left:0px;transform: translate3d(-51px, 34px, 0px); max-height:60vh;overflow:auto"></div>');
-      var scandropdown = $('#sortbyscan');
-      $.each(scanlations_groups, function(text){
-        var element= '<a id="{lang}" stuff="{minilang}" class="dropdown-item" href="#"></a>'
-        scandropdown.append(
-            $(element.replace(/{lang}/g, text).replace(/{minilang}/g, text)).html(text)
-        );
-        if(text == 'All'){
-            $('a[stuff="{minilang}"]'.replace(/{minilang}/g, text)).click(function(){
-                reverse();
-                console.log('done');
-            });
-        }else{
-            $('a[stuff="{minilang}"]'.replace(/{minilang}/g, text)).click(function(){
-                SortByScanlation($(this).attr('stuff'));
-            });
-        }
-      });
+    //   // inject Scanlation dropdown
+    //   $('div.col.order-lg-5 span.fas.fa-users').before('<div class="btn-group"><button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button><div>');
+    //   $('div.col.order-lg-5 button.btn.dropdown-toggle').append($('div.col.order-lg-5 span.fas.fa-users'));
+    //   $('div.col.order-lg-5 button.btn.dropdown-toggle span.fas.fa-users').after('<div  id="sortbyscan" class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position:absolute;will-change: transform; top:0px;left:0px;transform: translate3d(-51px, 34px, 0px); max-height:60vh;overflow:auto"></div>');
+    //   var scandropdown = $('#sortbyscan');
+    //   $.each(scanlations_groups, function(text){
+    //     var element= '<a id="{lang}" stuff="{minilang}" class="dropdown-item" href="#"></a>'
+    //     scandropdown.append(
+    //         $(element.replace(/{lang}/g, text).replace(/{minilang}/g, text)).html(text)
+    //     );
+    //     if(text == 'All'){
+    //         $('a[stuff="{minilang}"]'.replace(/{minilang}/g, text)).click(function(){
+    //             reverse();
+    //             console.log('done');
+    //         });
+    //     }else{
+    //         $('a[stuff="{minilang}"]'.replace(/{minilang}/g, text)).click(function(){
+    //             SortByScanlation($(this).attr('stuff'));
+    //         });
+    //     }
+    //   });
 
       function GetPageScanlations(){
           let dirty = [];
